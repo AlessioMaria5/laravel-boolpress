26f3a1aceed36d3816b646e8468af813113edd7e
AlessioMaria5 <alessiomaria5@gmail.com>
2022-10-18 16:42:18 +0200

    primo commit

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+
+use App\Http\Controllers\Controller;
+use App\Post;
+use App\Category;
+use App\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        
+        
+
+        return view('admin.posts.index',compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        $tags = Tag::all();
+
+        return view('admin.posts.create', compact('categories','tags'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'slug' =>  'unique:posts|required|max:50',
+            'category_id' => 'nullable|exists:categories,id',
+            'tags' => 'exists:tags,id'
+
+
+        ]);
+        
+        $postList = $request->all();
+
+
+        $post = new Post();
+        $post->fill($postList);
+
+        $post->save();
+        $post->tags()->sync($postList['tags']);
+
+        return redirect()->route('admin.posts.index')->with('status','nuovo post creato');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('admin.posts.show', ['post' => $post]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+            $tags = Tag::all();
+    
+        return view('admin.posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+       
+        $data = $request->all();
+
+        $post->update($data);
+        $post->save();
+        return redirect()->route('admin.posts.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        $post->tags()->sync([]);
+        return redirect()->route('admin.posts.index');
+    }
+
+    protected function filteredSlug($title){
+
+        $slug = Str::slug($title,'-');
+        return $slug; 
+    }
+
+
+}
